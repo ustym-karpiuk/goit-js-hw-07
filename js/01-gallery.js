d0fd9b9aa6bc2ba41677f5355af3ec3fde40569a
ustym-karpiuk <karpyuk0502@gmail.com>
2023-07-17 02:12:21 +0200

    1231231231231

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,63 +1,52 @@
 import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-//! Change code below this line
+// console.log(galleryItems);
 
-const galleryContainer = document.querySelector('.gallery');
-const galleryCardsSet = createGallery(galleryItems);
 
-function createGallery(galleryItems) {
-  return galleryItems
-    .map(({ original, preview, description }) => {
-      return `<div class="gallery__item" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
-    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
-  <a class="gallery__link" href="${original}" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
-    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-      style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
-    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)"
-    />
-  </a>
-</div>`;
-    })
+const galleryMarkUp = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+    .map(({ preview, description, original }) => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`)
     .join('');
-}
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
-galleryContainer.addEventListener('click', selectGalleryEl);
+galleryMarkUp.insertAdjacentHTML('beforeend', galleryEl)
 
-function selectGalleryEl(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}" width="800" height="600" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
-    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">`,
+galleryMarkUp.addEventListener('click', onImgClick)
 
-    {
-      onShow: () => {
-        window.addEventListener('keydown', onKeydownEsc);
-      },
-      onClose: () => {
-        window.removeEventListener('keydown', onKeydownEsc);
-      },
-    },
-  );
+function onImgClick(evt) {
+    evt.preventDefault();
 
-  // instance.show();
-
-  const onKeydownEsc = event => {
-    console.log(event.code);
-    if (event.code === 'Escape') {
-      instance.close();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
     }
-  };
-  // window.addEventListener('keydown', onKeydownEsc);
-  // window.removeEventListener('keydown', onKeydownEsc);
 
-  instance.show();
+    const modal = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+
+        {   onShow: () => window.addEventListener('keydown', onEscKeyPress),
+            onClose: () => window.removeEventListener('keydown', onEscKeyPress),
+        }
+    );
+    
+    modal.show();
+
+    function onEscKeyPress(evt) {   
+        if (evt.code === "Escape") {
+            modal.close();
+        }
+    }
 }
+
+
+
