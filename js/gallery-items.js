00caaad5b6b503eb7715fa64d8f122b30be7f547
ustym-karpiuk <karpyuk0502@gmail.com>
2023-07-17 12:00:39 +0200

    123123123123

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,9 +1,9 @@
 export const galleryItems = [
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
     description: 'Hokkaido Flower',
   },
   {
